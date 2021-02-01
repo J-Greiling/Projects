e20542855325b443b2d31f0cfe67cbaf3f3c938a
Jake <jakob.greiling@gmail.com>
2021-02-01 13:20:44 +0100

    intial commit

---
+++ Sorting_algorithms/bubble_sort.py
@@ -0,0 +1,39 @@
+"""Implementation of the Bubble sort alogrithm
+
+"""
+__author__ = "Jakob Greiling"
+__date__ = "2021-02"
+
+import numpy as np
+
+# function
+
+
+def bubble_sort(array: np.ndarray) -> np.ndarray:
+    '''Bubble sort Algorithm to sort array in ascending order
+
+    Parameters:
+    -----------
+    array: np.array
+        array to be sorted by this function
+
+    Returns:
+    array: np.array
+        sorted array
+
+    '''
+    len_array = array.size
+    for i in range(len_array):
+        for j in range(len_array - (i + 1)):
+            if(array[j] > array[j + 1]):
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
+
+
+# main Method
+if __name__ == '__main__':
+    array = np.array([1, -3, 2, 3, 7, 1], np.int32)
+    print(f"Array before sortig:\n {array}\n")
+    array = bubble_sort(array)
+    print(f"Array after sorting:\n {array}\n")
+
