2aafbae76133c4e8bfc0722277f01494f87a94e1
Jake <jakob.greiling@gmail.com>
2021-02-03 16:07:29 +0100

    changed .all() for basic string order test

--- sorting_algorithms/tests/test_pytest_bubblesort.py
+++ sorting_algorithms/tests/test_pytest_bubblesort.py
@@ -18,7 +18,7 @@ def test_sorting():
     array_in = np.array([2, 5, 1])
     array_out = np.array([1, 2, 5])
 
-    assert (array_out == bubble.sort(array_in)).all()
+    assert (array_out.all() == bubble.sort(array_in).all())
 
 
 def test_negative():
